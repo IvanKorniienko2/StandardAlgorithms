c9db88268301397ed199180321834389a27215d7
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2022-11-05 11:23:10 +0000

    added function

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -9,3 +9,14 @@ import Foundation
 
 print("Hello, Ivan!")
 
+let q = "how are you today"
+let message = "\(q)?"
+
+print(message)
+func go(number: Int) {
+    let a = number
+    print(a)
+}
+
+go(number: 17)
+
